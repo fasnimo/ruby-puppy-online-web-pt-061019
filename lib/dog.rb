248b06866754354d48c73edd02993bfdd81eb12b
Rene Cocom <fasnimo@gmail.com>
2019-07-16 08:23:21 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,44 @@
-# Add your code here
+class Dog
+  attr_accessor :name
+  @@all = []
+  @@names = []
+ def initialize(name)
+@name = name
+@@all << self
+@@names << name
+ end
+
+def self.all
+  @@all
+end
+
+def self.clear_all
+@@all.clear
+end
+
+def  self.print_all
+puts @@names.uniq
+end
+
+end
+# def self.new
+#   @@all << self
+# end
+
+# private
+#
+# def pluto
+#   @puppy.push("Pluto")
+# end
+#
+# def fido
+#   @puppy.push("Fido")
+# end
+#
+# def Maddy
+#   @puppy.push("Maddy")
+# end
+#
+#
+#
+# end
